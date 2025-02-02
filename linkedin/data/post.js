895709cf601d9f0e43fdce19c62db39ac9e2011d
Andyk <carlosandresriveravalle@gmail.com>
2025-02-02 22:53:15 +0000

    Position in navbar & copy + Change images profile

--- linkedin/data/post.js
+++ linkedin/data/post.js
@@ -1,14 +1,14 @@
 export const POSTS = [
   {
-    logoImgURL: "./img/icons/profile-icon.jpg",
-    logoAlt: "Andrés Rivera Valle Logo",
-    userName: "Andrés Rivera Valle",
-    userFollowers: "735 seguidores",
+    logoImgURL: "./img/icons/phyton-developers.jpg",
+    logoAlt: "Nielsen Norman Group Logo",
+    userName: "Python Web Developers",
+    userFollowers: "103.995 seguidores",
     postTime: "1 hora",
     postDescription:
-      "🚀 We took off with the first repliverse website! Inspired by the genius of Conikal and his original design for Deupload, I brought the main landing page to life based on his artwork and I can't wait to share future projects! Take the challenge to layout it with HTML, CSS and JS web base to improve my skills in these languages. 🎨",
-    postImagePrincipal: "./img/images/post-3.jpeg",
-    postImagePrincipalAlt: "Mockup deupload vista previa",
+      "🚀 30 AI Tools to Revolutionize Your Work in 2025! The future of work is here, and it’s powered by AI innovations. Whether you’re a developer, content creator, or business leader, these tools are designed to boost productivity, enhance creativity, and accelerate learning. 💡",
+    postImagePrincipal: "./img/images/post-4.gif",
+    postImagePrincipalAlt: "Visual desing glossary vista previa",
   },
   {
     logoImgURL: "./img/icons/nielsen-norman-group-logo.jpeg",
@@ -20,6 +20,17 @@ export const POSTS = [
       "Ever wonder how companies keep their designs consistent? It’s all thanks to design systems and style guides... but what's the difference?",
     postImagePrincipal: "./img/images/post-1.png",
     postImagePrincipalAlt: "Visual desing glossary vista previa",
+  },
+  {
+    logoImgURL: "./img/icons/profile-icon.jpg",
+    logoAlt: "Andrés Rivera Valle Logo",
+    userName: "Andrés Rivera Valle",
+    userFollowers: "735 seguidores",
+    postTime: "1 hora",
+    postDescription:
+      "🚀 We took off with the first repliverse website! Inspired by the genius of Conikal and his original design for Deupload, I brought the main landing page to life based on his artwork and I can't wait to share future projects! Take the challenge to layout it with HTML, CSS and JS web base to improve my skills in these languages. 🎨",
+    postImagePrincipal: "./img/images/post-3.jpeg",
+    postImagePrincipalAlt: "Mockup deupload vista previa",
   },
   {
     logoImgURL: "./img/icons/javascript-logo.jpeg",
